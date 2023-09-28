6bb948e5170fe7bb08e36af04d51efd98960934f
Rogix <12768403+rogix@users.noreply.github.com>
2023-09-27 21:53:42 -0300

    fix hero on wider screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,19 @@
 <section class="hero">
-	<div>Welcome to my website</div>
-	<h1>
-		Rogerio Pereira <br />
-		<span>Full-stack</span> developer<br /> from <span>Brazil</span>
-	</h1>
-	<h4>
-		This website is where you can discover my projects, resume, and insights. Feel free to contact <br
-		/>
-		me for questions or collaboration opportunities.
-	</h4>
-	<button class="btn btn-primary">Resume</button>
+	<div class="circle">
+		<div class="hero__content">
+			<h1>Welcome to my website</h1>
+			<h2>
+				Rogerio Pereira <br />
+				<span>Full-stack</span> developer<br /> from <span>Brazil</span>
+			</h2>
+			<p>
+				This website is where you can discover my projects, resume, and insights. Feel free to
+				contact <br />
+				me for questions or collaboration opportunities.
+			</p>
+			<button class="btn btn-primary">Resume</button>
+		</div>
+	</div>
 </section>
 
 <section class="about" id="about">
@@ -37,31 +41,53 @@
 
 <section class="experience" id="experience">I've worked..</section>
 
-<div class="circle" />
-
 <style>
+	:root {
+		--primary-color: #f57500;
+		--white-color: #ffffff;
+	}
+
 	.hero {
-		padding-left: 300px;
-		margin-block-start: 320px;
 		height: 100vh;
 	}
 
-	.hero > div {
+	.hero__content {
+		position: absolute;
+		left: 30%;
+		top: 15%;
+		width: 800px;
+	}
+
+	.hero__content h1 {
 		font-size: 11px;
 		font-weight: 300;
 		letter-spacing: 2px;
 		font-weight: 700;
-		color: #fff;
+		color: var(--white-color);
 		margin-bottom: 20px;
 		text-transform: uppercase;
 	}
 
-	.hero > h4 {
+	.hero__content h2 {
+		font-family: 'Oswald', sans-serif;
+		line-height: 62px;
+		color: var(--white-color);
+		font-size: 50px;
+		font-weight: bold;
+		text-transform: uppercase;
+		margin: 0;
+	}
+
+	.hero__content p {
 		font-size: 13px;
 		font-weight: 300;
 		font-weight: 700;
 		color: rgba(255, 255, 255, 0.51);
 		margin-bottom: 10px;
+	}
+
+	.hero__content span {
+		color: var(--primary-color);
 	}
 
 	.about {
@@ -75,8 +101,8 @@
 	}
 
 	.btn {
-		background-color: #f57500;
-		color: #fff;
+		background-color: var(--primary-color);
+		color: var(--white-color);
 		border: none;
 		padding: 18px 34px;
 		font-size: 12px;
@@ -87,22 +113,8 @@
 		margin-block-start: 10px;
 	}
 
-	h1 {
-		font-family: 'Oswald', sans-serif;
-		line-height: 62px;
-		color: #fff;
-		font-size: 50px;
-		font-weight: bold;
-		text-transform: uppercase;
-		margin: 0;
-	}
-
-	span {
-		color: #f57500;
-	}
-
 	.circle {
-		position: absolute;
+		position: relative;
 		width: 450px;
 		height: 450px;
 		border-radius: 50%;
@@ -114,8 +126,8 @@
 		margin-top: -245px;
 		z-index: -2;
 		background: rgba(255, 255, 255, 0.02);
-		border-right-color: #f57500;
-		border-left-color: #f57500;
+		border-right-color: var(--primary-color);
+		border-left-color: var(--primary-color);
 	}
 
 	.circle::before {
